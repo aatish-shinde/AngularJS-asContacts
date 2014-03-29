84c22d44200bcdaeced3f91ec38c371e69c062ff
aatish-shinde <aatish.shinde@gmail.com>
2014-03-29 17:39:27 -0400

    Update login.js

--- src/main/webapp/resources/js/pages/login.js
+++ src/main/webapp/resources/js/pages/login.js
@@ -1,4 +1,11 @@
-function loginController($scope, $location) {
-    var url = "" + $location.$$absUrl;
-    $scope.displayLoginError = (url.indexOf("error") >= 0);
-}
+(function() {
+    
+    
+    var as = angular.module('asApp.controllers', []);
+    
+    as.controller('loginController',['$scope', '$location', function ($scope, $location) {
+        var url = "" + $location.$$absUrl;
+        $scope.displayLoginError = (url.indexOf("error") >= 0);
+    }]);
+    
+}());
